3b0733bb38ad05642ec04c7ec34c7bd6c0cf90ad
Joel Georgie <joelvgeorgie@gmail.com>
2023-05-01 10:17:31 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask,render_template,request ,redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///task.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+db=SQLAlchemy(app)
+
+
+class Task(db.Model):
+    sno=db.Column(db.Integer,primary_key=True)
+    title=db.Column(db.String(200),nullable=False)
+    desc=db.Column(db.String(500),nullable=False)
+    date=db.Column(db.DateTime,default=datetime.utcnow)
+
+    def __repr__(self) -> str:
+        return f"{self.sno} - {self.title}"
+    
+
+with app.app_context():
+    db.create_all()   
+    
+@app.route("/",methods=["GET", "POST"])
+def task_assign():
+    if request.method == "POST":
+        title=request.form['title']
+        desc=request.form['desc']
+        todo=Task(title=title,desc=desc)
+        db.session.add(todo)
+        db.session.commit()
+    allTasks=Task.query.all()
+
+    return render_template("index.html",allTasks=allTasks)
+
+
+@app.route('/delete/<int:sno>')
+def delete(sno):
+    task = Task.query.filter_by(sno=sno).first()
+    db.session.delete(task)
+    db.session.commit()
+    return redirect("/")
+
+if __name__=="__main__":
+    app.run(debug=True,port=8000)
